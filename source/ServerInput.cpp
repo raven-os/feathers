06508789fce6e9607b7b5ae61e406ba91655d01d
riamon_v <vincent.riamon@epitech.eu>
2019-07-22 15:57:31 +0200

    Made things a bit more clearer, and omogeneous

--- source/ServerInput.cpp
+++ source/ServerInput.cpp
@@ -1,12 +1,12 @@
-#include "ServerInput.hpp"
+#include "InputManager.hpp"
 #include "Server.hpp"
 
-ServerInput::ServerInput() {
-  SET_LISTENER(ServerInput, ServerInputListeners, new_input, server_new_input);
+InputManager::InputManager() {
+  SET_LISTENER(InputManager, InputManagerListeners, new_input, server_new_input);
   wl_signal_add(&Server::getInstance().backend->events.new_input, &new_input);
 }
 
-void ServerInput::server_new_input([[maybe_unused]]struct wl_listener *listener, void *data)
+void InputManager::server_new_input([[maybe_unused]]struct wl_listener *listener, void *data)
 {
   struct wlr_input_device *device = static_cast<struct wlr_input_device *>(data);
   switch (device->type)
@@ -29,7 +29,7 @@ void ServerInput::server_new_input([[maybe_unused]]struct wl_listener *listener,
   wlr_seat_set_capabilities(Server::getInstance().seat.getSeat(), caps);
 }
 
-void ServerInput::server_new_keyboard(struct wlr_input_device *device)
+void InputManager::server_new_keyboard(struct wlr_input_device *device)
 {
   std::unique_ptr<Keyboard> keyboard(new Keyboard(device));
 
@@ -41,7 +41,7 @@ void ServerInput::server_new_keyboard(struct wlr_input_device *device)
   keyboards.emplace_back(std::move(keyboard));
 }
 
-void ServerInput::server_new_pointer(struct wlr_input_device *device)
+void InputManager::server_new_pointer(struct wlr_input_device *device)
 {
   wlr_cursor_attach_input_device(Server::getInstance().cursor.cursor, device);
 }
