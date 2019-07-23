44dd1c8af75792eccd5f13f9a7eda6cd05dafe99
David Zeng <david.zeng@epitech.eu>
2019-07-23 23:15:53 +0200

    Added primary selection

--- include/Seat.hpp
+++ include/Seat.hpp
@@ -8,6 +8,8 @@ class Server;
 struct SeatListeners
 {
   struct wl_listener request_cursor;
+  struct wl_listener request_set_selection;
+  struct wl_listener request_set_primary_selection;
 };
 
 class Seat : public SeatListeners
@@ -16,7 +18,10 @@ public:
   Seat();
   ~Seat() = default;
 
+  void seat_request_set_selection(struct wl_listener *listener, void *data);
+  void seat_request_set_primary_selection(struct wl_listener *listener, void *data);
   void seat_request_cursor(struct wl_listener *listener, void *data);
+
   struct wlr_seat *getSeat() const noexcept;
 
 private:
--- source/Seat.cpp
+++ source/Seat.cpp
@@ -4,8 +4,15 @@
 Seat::Seat()
 {
   seat = wlr_seat_create(Server::getInstance().getWlDisplay(), "seat0");
+
   SET_LISTENER(Seat, SeatListeners, request_cursor, seat_request_cursor);
   wl_signal_add(&seat->events.request_set_cursor, &request_cursor);
+
+  SET_LISTENER(Seat, SeatListeners, request_set_selection, seat_request_set_selection);
+  wl_signal_add(&seat->events.request_set_selection, &request_set_selection);
+
+  SET_LISTENER(Seat, SeatListeners, request_set_primary_selection, seat_request_set_primary_selection);
+  wl_signal_add(&seat->events.request_set_primary_selection, &request_set_primary_selection);
 }
 
 void Seat::seat_request_cursor([[maybe_unused]]struct wl_listener *listener, void *data)
@@ -16,6 +23,20 @@ void Seat::seat_request_cursor([[maybe_unused]]struct wl_listener *listener, voi
     {
       wlr_cursor_set_surface(Server::getInstance().cursor.cursor, event->surface, event->hotspot_x, event->hotspot_y);
     }
+}
+
+void Seat::seat_request_set_selection([[maybe_unused]]struct wl_listener *listener, void *data)
+{
+  struct wlr_seat_request_set_selection_event *event =
+    static_cast<struct wlr_seat_request_set_selection_event *>(data);
+  wlr_seat_set_selection(seat, event->source, event->serial);
+}
+
+void Seat::seat_request_set_primary_selection([[maybe_unused]]struct wl_listener *listener, void *data)
+{
+  struct wlr_seat_request_set_primary_selection_event *event =
+    static_cast<struct wlr_seat_request_set_primary_selection_event *>(data);
+  wlr_seat_set_primary_selection(seat, event->source, event->serial);
 }
 
 struct wlr_seat *Seat::getSeat() const noexcept
