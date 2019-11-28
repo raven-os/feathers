483070741dda928694b382472006b27f00bca234
Gaspard-- <jakob.kellendonk@epitech.eu>
2019-11-28 11:54:26 +0100

    Added `getTyped<Type>` and key repeat delay/rate configuration
    The `getType<Type>` will forward conversion to std::charconv which handles most basic types.

--- include/conf/Configuration.hpp
+++ include/conf/Configuration.hpp
@@ -4,6 +4,8 @@
 #include <memory>
 #include <filesystem>
 #include <unordered_map>
+#include <charconv>
+#include <cstring>
 
 namespace conf
 {
@@ -32,6 +34,24 @@ namespace conf
 
     /// wrapper over get to check a boolean setting
     bool getBool(char const *name);
+
+    /// wrapper over get to check an integer setting
+    template<class Type>
+    int getTyped(char const *name, Type defaultValue)
+    {
+      static_assert(!std::is_same_v<Type, bool> || !"Use getBool for boolean values!");
+      static_assert(!std::is_same_v<Type, char const *> || !"Use get for string settings!");
+      if (char const *value = get(name))
+	{
+	  Type result = defaultValue;
+	  auto [ptr, err] = std::from_chars(value, value + std::strlen(value), result);
+
+	  if (err == std::errc())
+	    return result;
+	  return defaultValue;
+	}
+      return defaultValue;
+    }
 
     /// get a setting only once, optimal when called rarely
     std::string getOnce(char const *name);
